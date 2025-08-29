e43922c0cb42fb50009fe406a6f2fe9271834209
FTCELISSON <elisson.junior@aluno.unex.edu.br>
2025-08-29 19:44:47 -0300

    Update Main.java
    Atualizado 29/08

--- main/src/Main.java
+++ main/src/Main.java
@@ -4,21 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       
-        Scanner scanner = new Scanner(System.in);
-        
-       
-        int escolha = menu(scanner);
-
-        
-        System.out.println("\nVocê escolheu a opção: " + escolha);
-        
-        scanner.close(); 
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean parar = false;
+            
+            do {
+                int opcao = menu(scanner);
+                
+                switch (opcao){
+                    
+                    case 9 -> {
+                        System.out.println("Encerrando o programa...");
+                        parar = true;
+                    }
+                    default -> System.out.println("inválida.");
+                }
+                
+            } while (!parar); 
+        }
     }
 
     public static int menu(Scanner scanner) {
-        System.out.println("Selecione:");
-
-        
+        System.out.println("\n--- MENU ---");
         System.out.println("[1] Cadastrar Novo Item");
         System.out.println("[2] Listar Itens do Cardápio");
         System.out.println("[3] Cadastrar Novo Cliente");
@@ -27,10 +33,10 @@ public class Main {
         System.out.println("[6] Atualizar Status de um Pedido");
         System.out.println("[7] Gerar Relatório de Vendas");
         System.out.println("[8] Gerar Relatório de Vendas (Detalhado)");
-
+        System.out.println("[9] Encerrar");
+        System.out.print("Escolha uma opção: ");
     
         int opcao = scanner.nextInt();
-
        
         return opcao;
     }
